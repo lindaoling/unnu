e685b1805b4b148a7ff96d74c5f8e449614eadf0
Halo <lindaoling@163.com>
2019-07-10 10:30:03 +0800

    bk

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,75 +1,47 @@
 <template>
   <div>
-    <mu-container style="background-color: #f5f5f5;">
-      <mu-appbar>
-        UNNU
-        <mu-button v-show="!mobileHeaderMenu" flat slot="right">Article</mu-button>
-        <mu-button v-show="!mobileHeaderMenu" flat slot="right">Photo</mu-button>
-      </mu-appbar>
-      <mu-card>
-        <mu-card-header title="Halo" sub-title="散落在指尖的阳光，我试着轻轻抓住光影的踪迹">
+    <div style="width: 100%;max-width:600px;margin: 0 auto;">
+      <mu-card style="width: 100%;margin: 0 auto;">
+        <mu-card-header title="Myron Avatar" sub-title="sub title">
           <mu-avatar slot="avatar">
             <img
-              src="https://unnu-1251996657.cos.ap-guangzhou.myqcloud.com/images/43914368_2388146701227914_8046520467531890451_n.jpg">
+              src="https://unnu-1251996657.cos.ap-guangzhou.myqcloud.com/images/44309794_761759400824774_6971370969521107371_n.jpg">
           </mu-avatar>
         </mu-card-header>
-        <mu-card-media>
+        <mu-card-media title="Image Title" sub-title="Image Sub Title">
           <img
             src="https://unnu-1251996657.cos.ap-guangzhou.myqcloud.com/images/44309794_761759400824774_6971370969521107371_n.jpg">
         </mu-card-media>
-        <mu-card-title title="Content Title" sub-title="sub title"></mu-card-title>
+        <mu-card-title title="Content Title" sub-title="Content Title"></mu-card-title>
         <mu-card-text>
-          散落在指尖的阳光，我试着轻轻抓
+          散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。
+          调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。
+          似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光，
+          找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
         </mu-card-text>
         <mu-card-actions>
-          <mu-button flat>人文</mu-button>
-          <mu-button flat>风景</mu-button>
+          <mu-button flat>Action 1</mu-button>
+          <mu-button flat>Action 2</mu-button>
         </mu-card-actions>
       </mu-card>
-    </mu-container>
+    </div>
   </div>
 </template>
 <script>
   export default {
     name: 'index',
     data() {
-      return {
-        docked: false,
-        open: false,
-        position: 'left',
-        mobileHeaderMenu: false,
-        windowInnerWidth: 0,
-        windowInnerHeight: 0
-      }
+      return {}
     },
-    created() {
-      // console.log(this.$style.red)
-      // -> "red_1VyoJ-uZ"
-      // 一个基于文件名和类名生成的标识符
-    },
-    methods: {
-      resize() {
-        // this.windowInnerWidth = window.innerWidth
-        // this.windowInnerHeight = window.innerHeight
-        // this.mobileHeaderMenu = this.windowInnerWidth < 600
-      }
-    }
+    created() {},
+    methods: {}
   }
 
 </script>
-
 <style lang="scss">
   .container {
-    padding-right: 0;
-    padding-left: 0;
-  }
-
-  .mu-card {
-    margin-bottom: 50px;
-  }
-
-  .mu-card-title-container {
-    padding: 16px 16px 0 16px;
+    padding: 0;
+    margin: 0;
   }
 
 </style>
