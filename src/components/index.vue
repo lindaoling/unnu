9c7996b3069c0946ff1f684ba0002d7aa0b2b8b6
Halo <lindaoling@163.com>
2019-07-15 17:40:52 +0800

    bk

--- src/components/index.vue
+++ src/components/index.vue
@@ -103,7 +103,9 @@
     padding: 2px 8px;
 
   }
-
+  .mu-carousel {
+    height: 400px;
+  }
   .mu-card {
     -webkit-box-shadow: 0 1px 1px -1px rgba(0, 0, 0, .1), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
     box-shadow: 0 1px 1px -1px rgba(0, 0, 0, .1), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
