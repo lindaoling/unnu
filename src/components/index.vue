3a9ff9387421728e6f64d435a64eebf875ef6608
Halo <lindaoling@163.com>
2019-07-15 17:37:05 +0800

    bk

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div style="background-color: #f5f5f5;border-bottom: 1px solid #d4d4d4">
-      <mu-appbar style="width: 100%;max-width:1000px;margin: 0 auto;" z-depth="0" title="UNNU">
+      <mu-appbar style="width: 100%;max-width:1000px;margin: 0 auto;background-color:transparent;" z-depth="0" title="UNNU">
         <mu-button flat slot="right">Photo</mu-button>
         <mu-button flat slot="right">Article</mu-button>
       </mu-appbar>
