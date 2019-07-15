a24dfd63f6d8522e011634da882ce874317fe64f
Halo <lindaoling@163.com>
2019-07-15 18:02:47 +0800

    bk

--- src/components/index.vue
+++ src/components/index.vue
@@ -108,6 +108,8 @@
     height: 0;
   }
   .mu-card {
+    // padding-bottom:100%;
+    // height: 0;
     -webkit-box-shadow: 0 1px 1px -1px rgba(0, 0, 0, .1), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
     box-shadow: 0 1px 1px -1px rgba(0, 0, 0, .1), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
   }
