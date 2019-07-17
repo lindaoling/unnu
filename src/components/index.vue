cf4d2efd251da1cbaae1d4119718e4e508eb405d
Halo <lindaoling@163.com>
2019-07-17 15:51:53 +0800

    bk

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,6 +2,9 @@
   <div>
     <div style="background-color: #f5f5f5;border-bottom: 1px solid #d4d4d4">
       <mu-appbar style="width: 100%;max-width:1000px;margin: 0 auto;background-color:transparent;" z-depth="0" title="UNNU">
+        <router-link to="/" style="color:rgb(115, 115, 115)">
+          unnu.me
+        </router-link>
         <mu-button flat slot="right">Photo</mu-button>
         <mu-button flat slot="right">Article</mu-button>
       </mu-appbar>
