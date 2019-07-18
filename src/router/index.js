446611d43ebe2a3bed731f9fbaf8e363ad1bbef3
Halo <lindaoling@163.com>
2019-07-18 13:54:37 +0800

    bk

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const _import = require('./_import_' + process.env.NODE_ENV)
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
