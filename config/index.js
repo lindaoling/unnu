b72a4e6025927beff5f94efa1ac26bce7b80df6e
lindaoling <398235105@qq.com>
2020-01-11 09:13:01 +0800

    bk

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://123.207.24.211:88866/',
 
     /**
      * Source Maps
