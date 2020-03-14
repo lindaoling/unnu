4c03fc87d13906ada7b8215b8f10775a4f54d093
lindaoling <398235105@qq.com>
2020-03-14 18:10:33 +0800

    bk

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,20 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created(){
+    let ismobile = this.isMobile()
+    if(ismobile){
+      console.log('issssssssssssssssssssss mobile')
+    }else{
+      console.log('issssssssssssssssssssss pc')
+    }
+  },
+  methods:{
+    isMobile() {
+      let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+      return flag;
+    }
+  }
 }
 </script>
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -10,12 +10,19 @@
 import Top from "./components/Top";
 import Foot from "./components/Foot";
 import BackToTop from "@/components/BackToTop";
+import ResizeMixin from './mixin/ResizeHandler'
 export default {
   name: 'Layout',
   components:{
     BackToTop,
     Top,
     Foot
+  },
+  mixins: [ResizeMixin],
+  methods:{
+    device() {
+      return this.$store.state.app.device
+    }
   }
 }
 </script>
