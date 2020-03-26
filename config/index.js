2704924570d5e6f88df7c803b337fcae43906f73
lindaoling <398235105@qq.com>
2020-03-26 15:58:07 +0800

    bk

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://123.207.24.211:8866/',
+    assetsPublicPath: '//augety.com/',
 
     /**
      * Source Maps
