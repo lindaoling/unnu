5b4a20517ff6599dd80ad35e35181266406c6aa5
lindaoling <398235105@qq.com>
2020-05-26 14:22:26 +0800

    bk

--- src/views/article/detail.vue
+++ src/views/article/detail.vue
@@ -3,30 +3,33 @@
       <div class="container-wrap">
         <div class="content-warp">
           <div class="article-content">
-            <h2>{{detail.title}}</h2>
-            <div class="" v-html="detail.content"></div>
-            <mu-flex align-items="center" style="margin-top:30px;">
-              <mu-flex justify-content="start" fill>上一篇：大国崛起</mu-flex>
-              <mu-flex justify-content="end" fill>上一篇：大国衰落</mu-flex>
-            </mu-flex>
+            <div class="thumbnail-box">
+              <img v-if=" '' != detail.thumbnail" :src="detail.thumbnail" alt="" style="max-width:100%">
+            </div>
+            <div class="main-editor">
+              <div class="title-box">
+                <h2>{{detail.title}}</h2>
+              </div>
+              <div class="content-box" v-html="detail.content"></div>
+              <mu-flex align-items="center" style="margin-top:30px;">
+                <mu-flex justify-content="start" fill>上一篇：大国崛起</mu-flex>
+                <mu-flex justify-content="end" fill>上一篇：大国衰落</mu-flex>
+              </mu-flex>
+            </div>
           </div>
         </div >
       </div >
-
   </mu-flex>
 </template>
+
 <script>
-  import {
-    read
-  } from "@/api/post"
-  import {
-    scrollTo
-  } from "@/utils/scrollTo"
+  import { read } from "@/api/post"
+  import { scrollTo } from "@/utils/scrollTo"
   export default {
     name: 'ArticleDetail',
     data() {
       return {
-        detail:{} //给子组合用的
+        detail:{}
       }
     },
     components: {
@@ -63,13 +66,28 @@
     background-color: #fff;
     max-width: 935px;    
     .content-warp{
-      padding: 30px 80px;
       .article-content{
         color: #333;
         font-family: Arial,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
         font-size: 16px;
         word-wrap: break-word;
         overflow: hidden;
+        .main-editor{padding: 0 80px 30px 80px;}
+        .thumbnail-box{}
+        .title-box{}
+        .content-box{}
+
+      }
+    }
+  }
+  @media screen and (max-width: 992px){
+    .container-wrap{
+      .content-warp{
+        .article-content{
+          .main-editor{
+            padding: 0 15px 10px 15px;
+          }
+        }
       }
     }
   }
