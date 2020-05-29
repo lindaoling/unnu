ccbb89739d57d75c03ba4ac8d2ce70a87d669b55
lindaoling <398235105@qq.com>
2020-05-29 15:15:10 +0800

    bk

--- src/views/article/detail.vue
+++ src/views/article/detail.vue
@@ -10,7 +10,6 @@
             <h2>{{detail.title}}</h2>
           </div>
           <div class="content-box markdown-body" v-html="detail.content"></div>
-          <!-- <div class="content-box markdown-body" >xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div> -->
           <mu-flex align-items="center" style="margin-top:30px;" fill>
             <mu-flex justify-content="start" fill>上一篇：大国崛起</mu-flex>
             <mu-flex justify-content="end" fill>上一篇：大国衰落</mu-flex>
@@ -49,7 +48,7 @@
       getRemoteDetail() {
         scrollTo(0)
         this.$progress.start()
-        const hash = this.$route.params.hash
+      const hash = this.$route.params.hash
         read(hash).then(response => {
           this.detail = response.data
           this.$progress.done()
@@ -74,20 +73,24 @@
           width: 100%;
         }
       }
-    }
-  }
-  .main-editor{
-    font-size: 16px;
-    word-wrap: break-word;
-    word-break:break-all; 
-    overflow: hidden;
-    padding: 0 80px 30px 80px;
-    .content-box{
-      /deep/ img{
-        max-width: 100% !important;
+      .main-editor{
+        font-size: 16px;
+        word-wrap: break-word;
+        word-break:break-all; 
+        overflow: hidden;
+        padding: 0 80px 30px 80px;
+        .content-box{
+          /deep/ img{
+            max-width: 100% !important;
+          }
+          /deep/ pre{
+            padding: 5px;
+          }
+        }
       }
     }
   }
+  
   @media screen and (max-width: 992px){
     .container-article-wrap{
       margin-top: 0;
@@ -98,4 +101,56 @@
       }
     }
   }
+
+  .hljs {
+    display: block;
+    overflow-x: auto;
+    padding: .5em;
+    background: #23241f
+  }
+
+  .hljs,.hljs-subst,.hljs-tag {
+    color: #f8f8f2
+  }
+
+  .hljs-emphasis,.hljs-strong {
+    color: #a8a8a2
+  }
+
+  .hljs-bullet,.hljs-link,.hljs-literal,.hljs-number,.hljs-quote,.hljs-regexp {
+    color: #ae81ff
+  }
+
+  .hljs-code,.hljs-section,.hljs-selector-class,.hljs-title {
+    color: #a6e22e
+  }
+
+  .hljs-strong {
+      font-weight: 700
+  }
+
+  .hljs-emphasis {
+      font-style: italic
+  }
+
+  .hljs-attr,.hljs-keyword,.hljs-name,.hljs-selector-tag {
+    color: #f92672
+  }
+
+  .hljs-attribute,.hljs-symbol {
+    color: #66d9ef
+  }
+
+  .hljs-class .hljs-title,.hljs-params {
+    color: #f8f8f2
+  }
+
+  .hljs-addition,.hljs-built_in,.hljs-builtin-name,.hljs-selector-attr,.hljs-selector-id,.hljs-selector-pseudo,.hljs-string,.hljs-template-variable,.hljs-type,.hljs-variable {
+    color: #e6db74
+  }
+
+  .hljs-comment,.hljs-deletion,.hljs-meta {
+    color: #75715e
+  }
+
 </style>
