c71726ad59fdb5723ff02338b83bddbf5ac28689
lindaoling <398235105@qq.com>
2020-05-29 14:49:13 +0800

    bk

--- src/views/article/detail.vue
+++ src/views/article/detail.vue
@@ -10,6 +10,7 @@
             <h2>{{detail.title}}</h2>
           </div>
           <div class="content-box markdown-body" v-html="detail.content"></div>
+          <!-- <div class="content-box markdown-body" >xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div> -->
           <mu-flex align-items="center" style="margin-top:30px;" fill>
             <mu-flex justify-content="start" fill>上一篇：大国崛起</mu-flex>
             <mu-flex justify-content="end" fill>上一篇：大国衰落</mu-flex>
@@ -66,40 +67,35 @@
     max-width: 935px;
     margin-top: 30px;
     .article-content{
-      max-width: 100%;
       color: #333;
       font-family: Arial,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
       .thumbnail-box {
-        width: 100%;
         img{
           width: 100%;
         }
       }
-      .main-editor{
-        font-size: 16px;
-        word-wrap: break-word;
-        word-break:break-all; 
-        overflow: hidden;
-        max-width: 100%;
-        width: 100%;
-        padding: 0 80px 30px 80px;
-        .content-box{
-          width: 100%;
-          /deep/ img{
-            max-width: 100% !important;
-          }
+    }
+  }
+  .main-editor{
+    font-size: 16px;
+    word-wrap: break-word;
+    word-break:break-all; 
+    overflow: hidden;
+    padding: 0 80px 30px 80px;
+    .content-box{
+      /deep/ img{
+        max-width: 100% !important;
+      }
+    }
+  }
+  @media screen and (max-width: 992px){
+    .container-article-wrap{
+      margin-top: 0;
+      .article-content{
+        .main-editor{
+          padding: 0 10px 10px 10px;
         }
       }
     }
   }
-  // @media screen and (max-width: 992px){
-  //   .container-article-wrap{
-  //     margin-top: 0;
-  //     .article-content{
-  //       .main-editor{
-  //         padding: 0 10px 10px 10px;
-  //       }
-  //     }
-  //   }
-  // }
 </style>
